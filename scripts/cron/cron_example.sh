525fdf96092336ac014148de20c41cae0e825372
Erik B Knudsen <erik.knudsen@copenhagenatomics.com>
2023-03-24 12:27:05 +0100

    example script to be put in a crontab

---
+++ scripts/cron/cron_example.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#get the github access token module
+ml gh_access
+
+cd Repos/Project_STATUS
+for script in `ls scripts`; do
+  if [ -d $script ]; then 
+    continue
+  elif [ -x $script ]; then
+    $script
+  else
+    python $script
+  fi
+done
+
+#update all the status tokens and push it upstream such that other projects can have access
+git commit -a
+git push
+
+#unload the access token module
+ml -gh_access
